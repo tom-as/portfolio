1b16c1c7fbb5e19d2266a7dbb82141bf78435e4d
Tomas <frcrewlapd@gmail.com>
2023-08-21 11:20:07 -0300

    Update

--- src/components/Intro/IntroFooter/introfooter.jsx
+++ src/components/Intro/IntroFooter/introfooter.jsx
@@ -3,7 +3,7 @@ const IntroFooter = () => {
 
   return (
     <footer className="flex justify-center align-baseline absolute inset-x-0 bottom-3">
-      <img className="animate-bounce hover:cursor-pointer max-h-30" src="src\assets\arrow.png" ></img>
+      <img className="animate-bounce hover:cursor-pointer max-h-30" src="./assets/arrow.png" ></img>
     </footer>
   )
 }
